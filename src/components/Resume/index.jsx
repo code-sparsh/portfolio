dbc283ee4f8daa05de7544e0778ded584754f346
Sparsh Sethi <sparshsethi15@gmail.com>
2024-03-21 21:19:52 +0530

    feat: add resume plugin

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,6 +1,19 @@
 const Resume = () => {
 
-    
+    return (
+
+        <div className=" h-full md:w-1/2 flex flex-col justify-center  gap-10 mx-8 md:mx-0">
+
+            <div className=" text-5xl text-yellow-200 text-center">Resume</div>
+
+            <iframe
+                src="https://drive.google.com/file/d/1fT8bM54nTL34pU5Bk-pXZlv4I9kAZLme/preview"
+                className="w-full h-4/5"
+                allow="autoplay"
+            ></iframe>
+
+        </div>
+    )
 }
 
 export default Resume;
